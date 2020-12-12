fdc28263cf4b959aa26563f0414f9090914c4508
pbairi37 <bairi.preeti@gmail.com>
2020-12-12 14:21:47 -0500

    session imports updated

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -15,6 +15,9 @@ from werkzeug.utils import secure_filename
 from datetime import datetime
 import os
 
+from flask import Flask, session
+from flask.ext.session import Session
+
 
 db = MongoEngine()
 login_manager = LoginManager()
@@ -29,6 +32,11 @@ def page_not_found(e):
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    SESSION_TYPE = 'redis'
+    app.config.from_object(__name__)
+    Session(app)
+    
     app.static_folder = 'static'
     app.config.from_pyfile("config.py", silent=False)
     if test_config is not None:
