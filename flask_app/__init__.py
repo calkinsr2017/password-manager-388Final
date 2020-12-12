c1e75d6df397a97d77027e52a8139bf113234b67
pbairi37 <bairi.preeti@gmail.com>
2020-12-12 14:41:07 -0500

    Update __init__.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,5 @@
 # 3rd-party packages
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mongoengine import MongoEngine
 from flask_login import (
     LoginManager,
@@ -14,6 +14,9 @@ from werkzeug.utils import secure_filename
 # stdlib
 from datetime import datetime
 import os
+
+from flask import Flask, session
+from flask_session import Session
 
 
 db = MongoEngine()
@@ -44,7 +47,9 @@ def create_app(test_config=None):
 
     login_manager.login_view = "users.login"
 
+    SESSION_TYPE = 'redis'
     app.config['SECRET_KEY'] = "Super secret key"
-    
+    app.config.from_object(__name__)
+    Session(app)
 
     return app
